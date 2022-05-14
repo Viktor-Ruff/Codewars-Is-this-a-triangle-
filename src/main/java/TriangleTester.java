7eeb12ede08039d48cb09ef0d6a815d02366c67c
Viktor-Ruff <HamiltonE.T.O>
2022-05-14 13:46:36 +0300

    first commit

---
+++ src/main/java/TriangleTester.java
@@ -0,0 +1,79 @@
+
+import java.text.DecimalFormat;
+
+class TriangleTester {
+
+    public boolean isTriangle(int a, int b, int c) {
+        boolean result = true;
+        double sumABC = 0;
+
+        // calculate the cosines of the angles of a triangle
+        try {
+            double A = (Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) / (2 * a * b);
+            double B = (Math.pow(b, 2) + Math.pow(c, 2) - Math.pow(a, 2)) / (2 * b * c);
+            double C = (Math.pow(a, 2) + Math.pow(c, 2) - Math.pow(b, 2)) / (2 * a * c);
+
+            // calculate the sum of three angles
+            sumABC = Math.acos(A) + Math.acos(B) + Math.acos(C);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // round to 14 decimal places and compare the result with pi
+        DecimalFormat decimalFormat = new DecimalFormat("#.##############");
+        String res = decimalFormat.format(sumABC);
+        String PI = decimalFormat.format(Math.PI);
+
+        if ((a <= 0 || b <= 0 || c <= 0) || !res.equalsIgnoreCase(PI)) {
+            result = false;
+        }
+
+        return result;
+    }
+}
+
+/*java.lang.AssertionError: expected:<false> but was:<true>
+	at org.junit.Assert.fail(Assert.java:89)
+	at org.junit.Assert.failNotEquals(Assert.java:835)
+	at org.junit.Assert.assertEquals(Assert.java:120)
+	at org.junit.Assert.assertEquals(Assert.java:146)
+	at TriangleTesterTest.changingSidesTest(TriangleTesterTest.java:14)
+	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+	at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:77)
+	at java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+	at java.base/java.lang.reflect.Method.invoke(Method.java:568)
+	at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:59)
+	at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
+	at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:56)
+	at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
+	at org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)
+	at org.junit.runners.BlockJUnit4ClassRunner$1.evaluate(BlockJUnit4ClassRunner.java:100)
+	at org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:366)
+	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:103)
+	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:63)
+	at org.junit.runners.ParentRunner$4.run(ParentRunner.java:331)
+	at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:79)
+	at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:329)
+	at org.junit.runners.ParentRunner.access$100(ParentRunner.java:66)
+	at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:293)
+	at org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)
+	at org.junit.runners.ParentRunner.run(ParentRunner.java:413)
+	at org.junit.runner.JUnitCore.run(JUnitCore.java:137)
+	at org.junit.runner.JUnitCore.run(JUnitCore.java:115)
+	at org.junit.vintage.engine.execution.RunnerExecutor.execute(RunnerExecutor.java:42)
+	at org.junit.vintage.engine.VintageTestEngine.executeAllChildren(VintageTestEngine.java:80)
+	at org.junit.vintage.engine.VintageTestEngine.execute(VintageTestEngine.java:72)
+	at org.junit.platform.launcher.core.EngineExecutionOrchestrator.execute(EngineExecutionOrchestrator.java:107)
+	at org.junit.platform.launcher.core.EngineExecutionOrchestrator.execute(EngineExecutionOrchestrator.java:88)
+	at org.junit.platform.launcher.core.EngineExecutionOrchestrator.lambda$execute$0(EngineExecutionOrchestrator.java:54)
+	at org.junit.platform.launcher.core.EngineExecutionOrchestrator.withInterceptedStreams(EngineExecutionOrchestrator.java:67)
+	at org.junit.platform.launcher.core.EngineExecutionOrchestrator.execute(EngineExecutionOrchestrator.java:52)
+	at org.junit.platform.launcher.core.DefaultLauncher.execute(DefaultLauncher.java:114)
+	at org.junit.platform.launcher.core.DefaultLauncher.execute(DefaultLauncher.java:86)
+	at org.junit.platform.launcher.core.DefaultLauncherSession$DelegatingLauncher.execute(DefaultLauncherSession.java:86)
+	at org.junit.platform.launcher.core.SessionPerRequestLauncher.execute(SessionPerRequestLauncher.java:53)
+	at com.codewars.junit5.TestRunner.executeTests(TestRunner.java:64)
+	at com.codewars.junit5.TestRunner.lambda$execute$0(TestRunner.java:56)
+	at com.codewars.junit5.TestRunner.callWithCustomClassLoader(TestRunner.java:147)
+	at com.codewars.junit5.TestRunner.execute(TestRunner.java:56)
+	at com.codewars.junit5.TestRunner.main(TestRunner.java:44)*/
---
+++ src/test/java/TriangleTesterTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTesterTest {
+
+    private TriangleTester triangleTester;
+
+    @BeforeEach
+    public void startUp() {
+        System.out.println("Test is START");
+        triangleTester = new TriangleTester();
+    }
+
+    @AfterEach
+    public void shutdown() {
+        System.out.println("Test is END");
+    }
+
+    @Test
+    void testTriangleTester1() {
+        Assertions.assertEquals(true, triangleTester.isTriangle(100, 100, 100));
+    }
+
+
+    @DisplayName("Check Exercise8")
+    @ParameterizedTest
+    @MethodSource("data")
+    public void paramTest8(boolean result, int a, int b, int c) {
+        Assertions.assertEquals(result, triangleTester.isTriangle(a, b, c));
+    }
+
+    static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.arguments(true, 5, 5, 7),
+                Arguments.arguments(true, 7, 5, 6),
+                Arguments.arguments(true, 18, 22, 19),
+                Arguments.arguments(true, 1, 1, 1),
+                Arguments.arguments(true, 126, 126, 15),
+                Arguments.arguments(true, 55, 51, 43),
+                Arguments.arguments(false, 2, 17, 283),
+                Arguments.arguments(false, 0, 2, 3),
+                Arguments.arguments(false, -35, 143, 32),
+                Arguments.arguments(false, 0, -15, -15),
+                Arguments.arguments(false, -2, -2, -2)
+        );
+    }
+}
